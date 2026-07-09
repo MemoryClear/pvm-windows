232b7a723858a402e61b5caed7ec474e1f633250
namejoe <memory_clear@qq.com>
2026-07-09 08:52:31 +0800

    bump version to v1.1.1 (remove broken lts shortcut)

--- install.ps1
+++ install.ps1
@@ -21,7 +21,7 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-$PVM_VERSION = '1.1.0'
+$PVM_VERSION = '1.1.2'
 $DEFAULT_INSTALL = Join-Path $env:USERPROFILE '.pvm'
 $SCRIPT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Path
 
